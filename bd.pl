b1be892bb14c95f2ab7f204a786a3c4cf773a049
onlymr <73614181+onlymr@users.noreply.github.com>
2022-04-15 17:09:34 +0100

    aa

--- bd.pl
+++ bd.pl
@@ -9,24 +9,35 @@
 %hotel1 - Burgus Tribute & Design Hotel
 %hotel2 - Luna Hotel
 %hotel3 - Hotel da Rocha
+%hotel4 - Hotel Covilha Dona Maria Affiliated by Meliá
+%hotel5 - Hotel Mestre de Avis
+%hotel6 - Melia Braga Hotel & Spa
+%hotel7 - MyStay Porto São Bento
 %hotel8 - Inatel Manteigas
-&casa4 - Casa Lagar Da Alagoa
+%hotel9 - Hotel Quinta das Lagrimas - Small Luxury
+%hotel10 - JR Studios & Suites I Rius
+%hotel11 - Borralha Hotel, Restaurante & Spa
+%hotel12 - Hotel do Mercado
+%hotel13 - Hotel das Salinas
+%casa1 - Bela Vila | Vila Hisbiscus
+%casa4 - Casa Lagar Da Alagoa
+
 &apartamento6 - Alojamento Pelourinho
 
 alojamento(hotel,'Burgus Tribute & Design Hotel',barato,alta,'braga',[atividade_descoberta],movimentado,[refeicoes_sim]).
 alojamento(hotel,'Luna Hotel',barato,alta,'serra da estrela',[atividade_maritima],reservado,[refeicoes_sim]).
 alojamento(hotel,'Hotel da Rocha',caro,alta,'algarve',[atividade_maritima],movimentado,[refeicoes_nao]).
-alojamento(hotel,hotel4,caro,baixa,'covilha',[atividade_trilhos],reservado,[refeicoes_sim]).
-alojamento(hotel,hotel5,medio,media,'guimarães',[atividade_descoberta],movimentado,[refeicoes_sim]).
-alojamento(hotel,hotel6,medio,baixa,'braga',[atividade_descoberta],movimentado,[refeicoes_sim]).
-alojamento(hotel,hotel7,barato,baixa,'porto',[atividade_descoberta],movimentado,[refeicoes_nao]).
+alojamento(hotel,'Hotel Covilha Dona Maria Affiliated by Meliá',caro,baixa,'covilha',[atividade_trilhos],reservado,[refeicoes_sim]).
+alojamento(hotel,'Hotel Mestre de Avis',medio,media,'guimarães',[atividade_descoberta],movimentado,[refeicoes_sim]).
+alojamento(hotel,'Melia Braga Hotel & Spa',medio,baixa,'braga',[atividade_descoberta],movimentado,[refeicoes_sim]).
+alojamento(hotel,'MyStay Porto São Bento',barato,baixa,'porto',[atividade_descoberta],movimentado,[refeicoes_nao]).
 alojamento(hotel,'Inatel Manteigas',barato,baixa,'serra da estrela',[atividade_trilhos],reservado,[refeicoes_sim]).
-alojamento(hotel,hotel9,caro,alta,'coimbra',[atividade_descoberta],reservado,[refeicoes_sim]).
-alojamento(hotel,hotel10,caro,alta,'porto',[atividade_descoberta, atividade_maritima],reservado,[refeicoes_sim]).
-alojamento(hotel,hotel11,barato,baixa,'vila real',[atividade_trilhos],movimentado,[refeicoes_sim]).
-alojamento(hotel,hotel12,medio,media,'aveiro',[atividade_maritima, atividade_descoberta],movimentado,[refeicoes_nao]).
-alojamento(hotel,hotel13,caro,media,'aveiro',[atividade_descoberta,atividade_maritima],reservado,[refeicoes_sim]).
-alojamento(casa,casa1,caro,alta,'algarve',[atividade_maritima],movimentado,[refeicoes_sim]).
+alojamento(hotel,'Hotel Quinta das Lagrimas - Small Luxury',caro,alta,'coimbra',[atividade_descoberta],reservado,[refeicoes_sim]).
+alojamento(hotel,'JR Studios & Suites I Rius',caro,alta,'porto',[atividade_descoberta, atividade_maritima],reservado,[refeicoes_sim]).
+alojamento(hotel,'Borralha Hotel, Restaurante & Spa',barato,baixa,'vila real',[atividade_trilhos],movimentado,[refeicoes_sim]).
+alojamento(hotel,'Hotel do Mercado',medio,media,'aveiro',[atividade_maritima, atividade_descoberta],movimentado,[refeicoes_nao]).
+alojamento(hotel,'Hotel das Salinas',caro,media,'aveiro',[atividade_descoberta,atividade_maritima],reservado,[refeicoes_sim]).
+alojamento(casa,'Bela Vila | Vila Hisbiscus',caro,alta,'algarve',[atividade_maritima],movimentado,[refeicoes_sim]).
 alojamento(casa,casa2,medio,baixa,'braga',[atividade_trilhos],reservado,[refeicoes_sim]).
 alojamento(casa,casa3,caro,alta,'coimbra',[atividade_descoberta],movimentado,[refeicoes_sim]).
 alojamento(casa,'Casa Lagar Da Alagoa',medio,baixa,'serra da estrela',[atividade_descoberta, atividade_trilhos],reservado,[refeicoes_nao]).
